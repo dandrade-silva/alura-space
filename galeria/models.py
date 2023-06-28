5911a3b0f9fa7cf67a4e30e5df8f7fd8b42830cc
Danilo Andrade <dandrade0803@gmail.com>
2023-06-28 19:36:38 -0300

    Curso 2 - Final Aula Avançando no Admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,16 +1,27 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 # Classe = Tabelas
 # Métodos = Campos
 
+categorias = [
+    ("NEBULOSA", "Nebulosa"),
+    ("ESTRELA", "Estrela"),
+    ("GALÁXIA", "Galáxia"),
+    ("PLANETA", "Planeta")
+]
+
 
 class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=categorias)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
