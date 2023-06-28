6a17568b79923c1a70279e6f2e8da460a56d5207
Danilo Andrade <dandrade0803@gmail.com>
2023-06-28 15:32:27 -0300

    Curso2 - Final da aula lidando com dados

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Classe = Tabelas
+# Métodos = Campos
+
+
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
