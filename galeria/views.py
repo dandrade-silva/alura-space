0cf3e407f8176ab79399040bfdaa3c3dabbed1b7
Danilo Andrade <dandrade0803@gmail.com>
2023-07-11 22:24:14 -0300

    Formação Django Alura - Final Curso02

--- galeria/views.py
+++ galeria/views.py
@@ -14,3 +14,14 @@ def index(request):
 def imagem(request, foto_id):
     foto = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'foto': foto})
+
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("nome").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_busca = request.GET['buscar']
+        if nome_busca:
+            fotografias = fotografias.filter(nome__icontains=nome_busca)
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
