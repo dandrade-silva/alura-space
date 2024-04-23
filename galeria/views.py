14342841a7b0a257289e3251d48da30c7ea2d66f
Danilo Andrade <dandrade0803@gmail.com>
2024-04-22 21:52:58 -0300

    Curso 3 Final Aula 1

--- galeria/views.py
+++ galeria/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
-# Create your views here.
-
 
 def index(request):
-
     fotografias = Fotografia.objects.order_by("nome").filter(publicada=True)
 
     return render(request, 'galeria/index.html', {"cards": fotografias})
